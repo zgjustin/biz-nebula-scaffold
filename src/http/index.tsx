96470921b8808a911f7776e713ac3dc18bd422d3
guijun.zhang <guijun.zhang@biz-united.com.cn>
2020-09-10 14:18:28 +0800

    update: 修改服务端接口端口

--- src/http/index.tsx
+++ src/http/index.tsx
@@ -1,5 +1,5 @@
 //配置 支持跨域的 api主机头
-const DOMAIN = "http://139.9.236.174:5900";
+const DOMAIN = "http://139.9.236.174:11000";
 //配置 api的根目录
 const PATH = "/v1"; //配置 api目录
 export default { DOMAIN, PATH };
