70cf24d11178584bb232489ae99019878d2b2fb4
guijun.zhang <guijun.zhang@biz-united.com.cn>
2020-09-11 11:34:38 +0800

    update: 添加外部页面路由示例

--- src/route.ts
+++ src/route.ts
@@ -1,14 +1,19 @@
 /*
  * @Author: justin
  * @Date: 2020-08-10 19:51:32
- * @LastEditTime: 2020-08-10 19:52:40
+ * @LastEditTime: 2020-09-11 11:34:14
  * @LastEditors: justin
- * @FilePath: /biz.nebula/nebula.scaffold/src/route.ts
+ * @FilePath: /biz-nebula-scaffold/src/route.ts
  * @Description: 路由配置
  */ 
 export default [
     {
         path:'/page1',
         component:'index'
+    },
+    {
+        path:'/page2',
+        component:'index',
+        outerPage:true // 不包含页面框架
     }
 ]
