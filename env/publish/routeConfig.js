0b10675209b6d15a470d8d361ef4361c7c43233f
guijun.zhang <guijun.zhang@biz-united.com.cn>
2020-11-27 18:09:20 +0800

    feat: 更新流程功能的流程类型和状态

--- env/publish/routeConfig.js
+++ env/publish/routeConfig.js
@@ -1,11 +1,13 @@
 /*
  * @Author: justin
  * @Date: 2020-11-23 07:12:00
- * @LastEditTime: 2020-11-23 07:14:46
+ * @LastEditTime: 2020-11-27 14:32:49
  * @LastEditors: justin
  * @FilePath: /nebula.first/env/publish/routeConfig.js
  * @Description: 
  */
 const path = require('path')
-require("ts-node/register");
+const tsConfig = require('../../tsconfig.json')
+tsConfig.compilerOptions.module='commonjs'
+require("ts-node").register(tsConfig);
 module.exports = require(path.join(process.cwd(), 'src/route.ts'));
